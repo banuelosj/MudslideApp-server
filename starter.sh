caefbf0dcaf99b2d36e33fe0bf9b0af9ee666786
Jinnan Zhang <vannizhang@gmail.com>
2019-01-11 21:04:20 -0800

    add starter.sh file

---
+++ starter.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+if [ $(ps -e -o uid,cmd | grep $UID | grep node | grep -v grep | wc -l | tr -s "\n") -eq 0 ]
+then
+        export PATH=/usr/local/bin:$PATH
+        forever start /root/MudslideApp-server/index.js >> /root/MudslideApp-server/log.txt 2>&1
+fi
